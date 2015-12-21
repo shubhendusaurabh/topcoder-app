048941494db1d53fed8e42fe75d74f7217069f1c
Nick Litwin <nlitwin@appirio.com>
2015-12-20 20:17:48 -0800

    Begin converting account submodule to ES6 and get ready for webpack

--- app/account/account.routes.js
+++ app/account/account.routes.js
@@ -1,128 +1,120 @@
-(function() {
-  'use strict';
+require('../../../topcoder-app/assets/images/logo_mobile.svg');
 
-  angular.module('tc.account').config(routes);
-
-  routes.$inject = ['$locationProvider', '$stateProvider', '$urlRouterProvider'];
-
-  function routes($locationProvider, $stateProvider, $urlRouterProvider) {
-    $locationProvider.html5Mode(true);
-
-    var states = {
-      'auth': {
-        parent: 'root',
-        abstract: true,
-        data: {
-          authRequired: false
-        },
-        onEnter: ['$state', '$stateParams', 'TcAuthService', function($state, $stateParams, TcAuthService) {
-          if (TcAuthService.isAuthenticated()) {
-            // redirect to next if exists else dashboard
-            if ($stateParams.next) {
-              $log.debug('Redirecting: ' + $stateParams.next);
-              window.location.href = decodeURIComponent($stateParams.next);
-            } else {
-              $state.go('dashboard');
-            }
-          }
-        }]
+export default function routes($locationProvider, $stateProvider, $urlRouterProvider) {
+  const states = {
+    auth: {
+      parent: 'root',
+      abstract: true,
+      data: {
+        authRequired: false
       },
-      'login': {
-        parent: 'auth',
-        url: '/login/?next&code&state&status&userJWTToken&utm_source&utm_medium&utm_campaign',
-        params: { 'notifyReset': false },
-        data: {
-          title: 'Login'
-        },
-        views: {
-          'header@': {
-            templateUrl: 'layout/header/account-header.html'
-          },
-          'container@': {
-            templateUrl: 'account/login/login.html',
-            controller: 'LoginController',
-            controllerAs: 'vm'
-          },
-          'footer@': {
-            templateUrl: 'layout/footer/account-footer.html'
+      onEnter: ['$state', '$stateParams', 'TcAuthService', function($state, $stateParams, TcAuthService) {
+        if (TcAuthService.isAuthenticated()) {
+          // redirect to next if exists else dashboard
+          if ($stateParams.next) {
+            $log.debug('Redirecting: ' + $stateParams.next);
+            window.location.href = decodeURIComponent($stateParams.next);
+          } else {
+            $state.go('dashboard');
           }
         }
+      }]
+    },
+    login: {
+      parent: 'auth',
+      url: '/login/?next&code&state&status&userJWTToken&utm_source&utm_medium&utm_campaign',
+      params: { 'notifyReset': false },
+      data: {
+        title: 'Login'
       },
-      'register': {
-        parent: 'auth',
-        url: '/register/?next&utm_source&utm_medium&utm_campaign',
-        data: {
-          title: "Join"
+      views: {
+        'header@': {
+          template: require('../layout/header/account-header')(),
         },
-        views: {
-          'header@': {
-            templateUrl: 'layout/header/account-header.html'
-          },
-          'container@': {
-            templateUrl: 'account/register/register.html',
-            controller: 'RegisterController',
-            controllerAs: 'vm'
-          },
-          'footer@': {
-            templateUrl: 'layout/footer/account-footer.html'
-          }
-        }
-      },
-      'registeredSuccessfully': {
-        url: '/registered-successfully/',
-        data: {
-          title: 'Registered',
-          authRequired: false
+        'container@': {
+          template: require('../account/login/login')(),
+          controller: 'LoginController',
+          controllerAs: 'vm'
         },
-        views: {
-          'header@': {
-            templateUrl: 'layout/header/account-header.html'
-          },
-          'container@': {
-            templateUrl: 'account/register/registered-successfully.html'
-          },
-          'footer@': {
-            templateUrl: 'layout/footer/account-footer.html'
-          }
-        }
-      },
-      'resetPassword': {
-        parent: 'auth',
-        url: '/reset-password/?token&handle',
-        data: {
-          title: "Reset Password"
-        },
-        views: {
-          'header@': {
-            templateUrl: 'layout/header/account-header.html'
-          },
-          'container@': {
-            templateUrl: 'account/reset-password/reset-password.html',
-            controller: 'ResetPasswordController',
-            controllerAs: 'vm'
-          },
-          'footer@': {
-            templateUrl: 'layout/footer/account-footer.html'
-          }
-        }
-      },
-      logout: {
-        url: '/logout/',
-        views: {
-          'header@': {},
-          'container@': {
-            controller: 'LogoutController'
-          },
-          'footer@': {}
-        },
-        data: {
-          authRequired: false
+        'footer@': {
+          template: require('../layout/footer/account-footer')(),
         }
       }
-    };
+    },
+    register: {
+      parent: 'auth',
+      url: '/register/?next&utm_source&utm_medium&utm_campaign',
+      data: {
+        title: "Join"
+      },
+      views: {
+        'header@': {
+          template: require('../layout/header/account-header')(),
+        },
+        'container@': {
+          template: require('../account/register/register')(),
+          controller: 'RegisterController',
+          controllerAs: 'vm'
+        },
+        'footer@': {
+          template: require('../layout/footer/account-footer')(),
+        }
+      }
+    },
+    registeredSuccessfully: {
+      url: '/registered-successfully/',
+      data: {
+        title: 'Registered',
+        authRequired: false
+      },
+      views: {
+        'header@': {
+          template: require('../layout/header/account-header')(),
+        },
+        'container@': {
+          template: require('../account/register/registered-successfully')(),
+        },
+        'footer@': {
+          template: require('../layout/footer/account-footer')(),
+        }
+      }
+    },
+    resetPassword: {
+      parent: 'auth',
+      url: '/reset-password/?token&handle',
+      data: {
+        title: "Reset Password"
+      },
+      views: {
+        'header@': {
+          template: require('../layout/header/account-header')(),
+        },
+        'container@': {
+          template: require('../account/reset-password/reset-password')(),
+          controller: 'ResetPasswordController',
+          controllerAs: 'vm'
+        },
+        'footer@': {
+          template: require('../layout/footer/account-footer')(),
+        }
+      }
+    },
+    logout: {
+      url: '/logout/',
+      views: {
+        'header@': {},
+        'container@': {
+          controller: 'LogoutController'
+        },
+        'footer@': {}
+      },
+      data: {
+        authRequired: false
+      }
+    }
+  };
 
-    angular.forEach(states, function(state, name) {
-      $stateProvider.state(name, state);
-    });
-  }
-})();
+  angular.forEach(states, function(state, name) {
+    $stateProvider.state(name, state)
+  })
+}
--- app/account/login/login.controller.js
+++ app/account/login/login.controller.js
@@ -1,133 +1,127 @@
-(function() {
-  'use strict';
+// import './login/login.scss'
 
-  angular.module('tc.account').controller('LoginController', LoginController);
+export default function LoginController($log, $state, $stateParams, $location, $scope, TcAuthService, UserService, Helpers, CONSTANTS) {
+  var vm = this;
 
-  LoginController.$inject = ['$log', '$state', '$stateParams', '$location', '$scope', 'TcAuthService', 'UserService', 'NotificationService', 'Helpers', 'CONSTANTS'];
+  // $log = $log.getInstance("LoginController");
+  vm.$stateParams = $stateParams;
+  vm.passwordReset = false;
+  vm.loginErrors = {
+    USERNAME_NONEXISTANT: false,
+    WRONG_PASSWORD: false,
+    SOCIAL_LOGIN_ERROR: false
+  };
 
-  function LoginController($log, $state, $stateParams, $location, $scope, TcAuthService, UserService, NotificationService, Helpers, CONSTANTS) {
-    var vm = this;
-    $log = $log.getInstance("LoginController");
-    vm.$stateParams = $stateParams;
-    vm.passwordReset = false;
-    vm.loginErrors = {
-      USERNAME_NONEXISTANT: false,
-      WRONG_PASSWORD: false,
-      SOCIAL_LOGIN_ERROR: false
-    };
+  vm.login = login;
+  vm.socialLogin = socialLogin;
 
-    vm.login = login;
-    vm.socialLogin = socialLogin;
+  // reference for main vm
+  var mainVm = $scope.$parent.main;
 
-    // reference for main vm
-    var mainVm = $scope.$parent.main;
+  activate();
 
-    activate();
-
-    function activate() {
-      if ($stateParams.notifyReset) {
-        NotificationService.inform('Your new password has been set. Please log in. If you have any trouble, please contact support@topcoder.com.');
-      }
-    }
-
-    function login() {
-      vm.loginErrors.USERNAME_NONEXISTANT = false;
-      vm.loginErrors.WRONG_PASSWORD = false;
-
-      // TODO ideally it should be done by dedicated directive to handle all outside clicks
-      mainVm.menuVisible = false;
-
-      if (Helpers.isEmail(vm.username)) {
-        // the user is loggin in using email
-        vm.emailOrUsername = 'email';
-
-        // ensure email exists
-        // uses same validity check as registration
-        // valid => email isn't already used by someone
-        UserService.validateUserEmail(vm.username).then(function(data) {
-          if (data.valid) {
-            // email doesn't exist
-            vm.loginErrors.USERNAME_NONEXISTANT = true;
-          } else {
-            _doLogin(vm.username, vm.currentPassword);
-          }
-        }).catch(function(resp) {
-          // TODO handle error
-          // assume email exists, login would in any case if it didn't
-          vm.loginErrors.USERNAME_NONEXISTANT = false;
-          _doLogin(vm.username, vm.currentPassword);
-        });
-      } else {
-        // the user is logging in using a username
-        vm.emailOrUsername = 'username';
-
-        // username - make sure it exists
-        UserService.validateUserHandle(vm.username).then(function(data) {
-          if (data.valid) {
-            // username doesn't exist
-            vm.loginErrors.USERNAME_NONEXISTANT = true;
-          } else {
-            _doLogin(vm.username, vm.currentPassword);
-          }
-        }).catch(function(resp) {
-          // TODO handle error
-          // assume email exists, login would in any case if it didn't
-          _doLogin(vm.username, vm.currentPassword);
-        });
-      }
-    };
-
-    function _doLogin(usernameOrEmail, password) {
-      return TcAuthService.login(usernameOrEmail, password).then(function(data) {
-        // success
-        $log.debug('logged in');
-
-        // setup login event for analytics tracking
-        Helpers.setupLoginEventMetrics(usernameOrEmail);
-        return Helpers.redirectPostLogin($stateParams.next);
-
-      }).catch(function(resp) {
-        $log.warn(resp);
-        switch (resp.status) {
-          case "ACCOUNT_INACTIVE":
-            $state.go('registeredSuccessfully');
-            // user should already be redirected
-            break;
-          case "UNKNOWN_ERROR":
-          default:
-            vm.loginErrors.WRONG_PASSWORD = true;
-            vm.password = '';
-        }
-      });
-    }
-
-    function socialLogin(platform) {
-      // we need to pass on the 'next' param if we have one
-      var params = {};
-      if ($stateParams.next) {
-        params = {next: $stateParams.next};
-      }
-
-      // redirect back to login
-      var callbackUrl = $state.href('login', params, {absolute: true});
-
-      TcAuthService.socialLogin(platform, callbackUrl)
-      .then(function() {
-        $log.debug('logged in');
-        return Helpers.redirectPostLogin($stateParams.next);
-      })
-      .catch(function(resp) {
-        switch (resp.status) {
-          case "ACCOUNT_INACTIVE":
-            window.location.href = "https://www." + CONSTANTS.domain + "/account-inactive/";
-          case "USER_NOT_REGISTERED":
-          default:
-            vm.socialLoginError = 401;
-            vm.loginErrors.SOCIAL_LOGIN_ERROR = true;
-            break;
-        }
-      });
-    };
+  function activate() {
+    // if ($stateParams.notifyReset) {
+    //   NotificationService.inform('Your new password has been set. Please log in. If you have any trouble, please contact support@topcoder.com.');
+    // }
   }
 
-})();
+  function login() {
+    vm.loginErrors.USERNAME_NONEXISTANT = false;
+    vm.loginErrors.WRONG_PASSWORD = false;
+
+    // TODO ideally it should be done by dedicated directive to handle all outside clicks
+    mainVm.menuVisible = false;
+
+    if (Helpers.isEmail(vm.username)) {
+      // the user is loggin in using email
+      vm.emailOrUsername = 'email';
+
+      // ensure email exists
+      // uses same validity check as registration
+      // valid => email isn't already used by someone
+      UserService.validateUserEmail(vm.username).then(function(data) {
+        if (data.valid) {
+          // email doesn't exist
+          vm.loginErrors.USERNAME_NONEXISTANT = true;
+        } else {
+          _doLogin(vm.username, vm.currentPassword);
+        }
+      }).catch(function(resp) {
+        // TODO handle error
+        // assume email exists, login would in any case if it didn't
+        vm.loginErrors.USERNAME_NONEXISTANT = false;
+        _doLogin(vm.username, vm.currentPassword);
+      });
+    } else {
+      // the user is logging in using a username
+      vm.emailOrUsername = 'username';
+
+      // username - make sure it exists
+      UserService.validateUserHandle(vm.username).then(function(data) {
+        if (data.valid) {
+          // username doesn't exist
+          vm.loginErrors.USERNAME_NONEXISTANT = true;
+        } else {
+          _doLogin(vm.username, vm.currentPassword);
+        }
+      }).catch(function(resp) {
+        // TODO handle error
+        // assume email exists, login would in any case if it didn't
+        _doLogin(vm.username, vm.currentPassword);
+      });
+    }
+  };
+
+  function _doLogin(usernameOrEmail, password) {
+    return TcAuthService.login(usernameOrEmail, password).then(function(data) {
+      // success
+      $log.debug('logged in');
+
+      // setup login event for analytics tracking
+      Helpers.setupLoginEventMetrics(usernameOrEmail);
+      return Helpers.redirectPostLogin($stateParams.next);
+
+    }).catch(function(resp) {
+      $log.warn(resp);
+      switch (resp.status) {
+        case "ACCOUNT_INACTIVE":
+          $state.go('registeredSuccessfully');
+          // user should already be redirected
+          break;
+        case "UNKNOWN_ERROR":
+        default:
+          vm.loginErrors.WRONG_PASSWORD = true;
+          vm.password = '';
+      }
+    });
+  }
+
+  function socialLogin(platform) {
+    // we need to pass on the 'next' param if we have one
+    var params = {};
+    if ($stateParams.next) {
+      params = {next: $stateParams.next};
+    }
+
+    // redirect back to login
+    var callbackUrl = $state.href('login', params, {absolute: true});
+
+    TcAuthService.socialLogin(platform, callbackUrl)
+    .then(function() {
+      $log.debug('logged in');
+      return Helpers.redirectPostLogin($stateParams.next);
+    })
+    .catch(function(resp) {
+      switch (resp.status) {
+        case "ACCOUNT_INACTIVE":
+          window.location.href = "https://www." + CONSTANTS.domain + "/account-inactive/";
+        case "USER_NOT_REGISTERED":
+        default:
+          vm.socialLoginError = 401;
+          vm.loginErrors.SOCIAL_LOGIN_ERROR = true;
+          break;
+      }
+    });
+  };
+}
