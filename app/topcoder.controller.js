827d007ce324f2605041edcead61f601bee6455c
Nick Litwin <nlitwin@appirio.com>
2015-12-20 20:19:56 -0800

    Comment out during refactor

--- app/topcoder.controller.js
+++ app/topcoder.controller.js
@@ -1,48 +1,40 @@
-(function() {
-  'use strict';
+export default function TopcoderController ($rootScope, $document, CONSTANTS, IntroService, $timeout) {
+  var vm = this;
 
-  angular.module('topcoder').controller('TopcoderController', TopcoderController);
+  activate();
 
-  TopcoderController.$inject = ['NotificationService', '$rootScope', '$document', 'CONSTANTS', 'IntroService', '$timeout'];
+  function activate() {
+    $rootScope.DOMAIN = CONSTANTS.domain;
+    vm.menuVisible = false;
+    vm.globalToasterConfig = {
+      'close-button': {
+        'toast-warning': true,
+        'toast-error': true,
+        'toast-success': false
+      },
+      'body-output-type': 'trustedHtml',
+      'position-class': 'toast-top-center'
+    };
 
-  function TopcoderController(NotificationService, $rootScope, $document, CONSTANTS, IntroService, $timeout) {
-    var vm = this;
-
-    activate();
-
-    function activate() {
-      $rootScope.DOMAIN = CONSTANTS.domain;
+    $rootScope.$on('$stateChangeStart', function() {
       vm.menuVisible = false;
-      vm.globalToasterConfig = {
-        'close-button': {
-          'toast-warning': true,
-          'toast-error': true,
-          'toast-success': false
-        },
-        'body-output-type': 'trustedHtml',
-        'position-class': 'toast-top-center'
-      };
+    });
 
-      $rootScope.$on('$stateChangeStart', function() {
-        vm.menuVisible = false;
-      });
+    $rootScope.$on('$stateChangeSuccess', function(evt, toState, toParams, fromState, fromParams) {
+      $document[0].body.scrollTop = $document[0].documentElement.scrollTop = 0;
 
-      $rootScope.$on('$stateChangeSuccess', function(evt, toState, toParams, fromState, fromParams) {
-        $document[0].body.scrollTop = $document[0].documentElement.scrollTop = 0;
+      vm.introOptions = IntroService.getCurrentPageOptions();
 
-        vm.introOptions = IntroService.getCurrentPageOptions();
+      $timeout(function() {
+        if (vm.introOptions) {
+          vm.startIntro();
+        }
+      }, 0);
+    });
 
-        $timeout(function() {
-          if (vm.introOptions) {
-            vm.startIntro();
-          }
-        }, 0);
-      });
-
-      // TODO - enable this once we support notificaitons
-      // $rootScope.$on(CONSTANTS.EVENT_USER_LOGGED_IN, function() {
-      //   NotificationService.getNotifications();
-      // });
-    }
+    // TODO - enable this once we support notificaitons
+    // $rootScope.$on(CONSTANTS.EVENT_USER_LOGGED_IN, function() {
+    //   NotificationService.getNotifications();
+    // });
   }
-})();
+}
--- app/topcoder.module.js
+++ app/topcoder.module.js
@@ -1,81 +1,73 @@
-(function() {
-  'use strict';
+var dependencies = [
+  'tc.services',
+  'tc.layout',
+  'tc.account',
+  'tc.peer-review',
+  'tc.myDashboard',
+  'tc.mySRMs',
+  'tc.myChallenges',
+  'tc.profile',
+  'tc.settings',
+  'tc.submissions',
+  'tc.skill-picker',
+  'tc.sitemap',
+  'tc.community',
+  uiRouter,
+  'blocks.logger',
+  'blocks.exception',
+  'ngCookies',
+  'angular-storage',
+  'restangular',
+  'ngNotificationsBar',
+  'ngSanitize',
+  'ngDropdowns',
+  'ngDialog',
+  'xml',
+  'angular.filter',
+  'CONSTANTS',
+  'dcbImgFallback',
+  'toaster',
+  'angular-intro',
+  'ngMessages',
+  'angular-carousel',
+  'sticky',
+  'dibari.angular-ellipsis'
+];
 
-  var dependencies = [
-    'tc.services',
-    'tc.layout',
-    'tc.account',
-    'tc.peer-review',
-    'tc.myDashboard',
-    'tc.mySRMs',
-    'tc.myChallenges',
-    'tc.profile',
-    'tc.settings',
-    'tc.submissions',
-    'tc.skill-picker',
-    'tc.sitemap',
-    'tc.community',
-    'ui.router',
-    'blocks.logger',
-    'blocks.exception',
-    'ngCookies',
-    'angular-storage',
-    'restangular',
-    'ngNotificationsBar',
-    'ngSanitize',
-    'ngDropdowns',
-    'ngDialog',
-    // 'xml',
-    'angular.filter',
-    'CONSTANTS',
-    'dcbImgFallback',
-    'toaster',
-    // 'angular-intro',
-    'ngMessages',
-    'angular-carousel',
-    'sticky',
-    'dibari.angular-ellipsis'
-  ];
+angular.module('topcoder', dependencies);
+.run(appRun);
 
-  angular.module('topcoder', dependencies).run(appRun);
+appRun.$inject = ['$rootScope', '$state', 'TcAuthService', '$cookies', 'Helpers', '$log', 'NotificationService', 'CONSTANTS'];
 
-  appRun.$inject = ['$rootScope', '$state', 'TcAuthService', '$cookies', 'Helpers', '$log', 'NotificationService', 'CONSTANTS'];
+function appRun($rootScope, $state, TcAuthService, $cookies, Helpers, $log, NotificationService, CONSTANTS) {
+  // Check AuthNAuth on change state start
+  $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+    if (toState.data.authRequired && !TcAuthService.isAuthenticated()) {
+      $log.debug('State requires authentication, and user is not logged in, redirecting');
+      // Setup redirect for post login
+      event.preventDefault();
+      var next = $state.href(toState.name, toParams, {absolute: false});
+      $state.go('login', {next: next});
+    }
+  });
 
-  function appRun($rootScope, $state, TcAuthService, $cookies, Helpers, $log, NotificationService, CONSTANTS) {
-    // Attaching $state to the $rootScope allows us to access the
-    // current state in index.html (see the body tag)
-    $rootScope.$state = $state;
+  $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+    // Set document title
+    document.title = Helpers.getPageTitle(toState, $state.$current);
+    // Adds previous state to scope
+    $rootScope.previousState = fromState;
+  });
 
-    // check AuthNAuth on change state start
-    $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
-      if (toState.data.authRequired && !TcAuthService.isAuthenticated()) {
-        $log.debug('State requires authentication, and user is not logged in, redirecting');
-        // setup redirect for post login
-        event.preventDefault();
-        var next = $state.href(toState.name, toParams, {absolute: false});
-        $state.go('login', {next: next});
-      }
-    });
+  $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
+    console.log.bind(console);
+  });
 
-    $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
-      // set document title
-      document.title = Helpers.getPageTitle(toState, $state.$current);
-      // adds previous state to scope
-      $rootScope.previousState = fromState;
-    });
+  // NotificationService.getNotifications();
+}
 
-    $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
-      console.log.bind(console);
-    });
-
-    // NotificationService.getNotifications();
-  }
-
-  angular.module('topcoder').config(['$httpProvider', 'RestangularProvider', '$locationProvider',
-   function($httpProvider, RestangularProvider, $locationProvider) {
-    $locationProvider.html5Mode(true);
-    $httpProvider.interceptors.push('HeaderInterceptor');
-    RestangularProvider.setRequestSuffix('/');
-  }]);
-
-})();
+angular.module('topcoder').config(['$httpProvider', 'RestangularProvider', '$locationProvider',
+ function($httpProvider, RestangularProvider, $locationProvider) {
+  $locationProvider.html5Mode(true);
+  $httpProvider.interceptors.push('HeaderInterceptor');
+  RestangularProvider.setRequestSuffix('/');
+}]);
